533133de99608381664af283b7eb9428b9f4f821
rehan <rehanmir30997@gmail.com>
2021-04-29 15:12:12 +0500

    updated

--- app/src/main/java/com/example/cupid/Fragments/DiscoverFragment.java
+++ app/src/main/java/com/example/cupid/Fragments/DiscoverFragment.java
@@ -1,5 +1,6 @@
 package com.example.cupid.Fragments;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,8 +10,27 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.cupid.Adapter.CardsAdapter;
+import com.example.cupid.Model.CardItem;
 import com.example.cupid.R;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import link.fls.swipestack.SwipeStack;
+
+import static android.content.Context.MODE_PRIVATE;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,6 +38,12 @@ import com.example.cupid.R;
  * create an instance of this fragment.
  */
 public class DiscoverFragment extends Fragment {
+
+    private SwipeStack cardStack;
+    private CardsAdapter cardsAdapter;
+    private ArrayList<CardItem> cardItems;
+    private int currentPosition;
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -65,18 +91,59 @@ public class DiscoverFragment extends Fragment {
 
         // Inflate the layout for this fragment
         View v=inflater.inflate(R.layout.fragment_discover, container, false);
-        FrameLayout frameLayout=v.findViewById(R.id.matches_frame);
 
-        MatchesFragment matchesFragment=new MatchesFragment();
-        setfragment(matchesFragment);
+
+
+        cardStack = v.findViewById(R.id.container);
+        setCardStackAdapter();
+        currentPosition = 0;
+//        FrameLayout frameLayout=v.findViewById(R.id.matches_frame);
+//
+//        MatchesFragment matchesFragment=new MatchesFragment();
+//        setfragment(matchesFragment);
 
         return v;
     }
-    private void setfragment(Fragment page) {
 
-        FragmentTransaction fragmentTransaction =getFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.matches_frame,page);
-        fragmentTransaction.commit();
+    private void setCardStackAdapter() {
 
+
+        String url="http://api.betterdate.info/endpoints/user.php";
+        RequestQueue queue1 = Volley.newRequestQueue(getContext());
+        cardItems = new ArrayList<>();
+
+        JsonArrayRequest request=new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        queue1.add(request);
+
+
+
+//        cardItems.add(new CardItem(R.drawable.a, "Huyen My", "Hanoi"));
+//        cardItems.add(new CardItem(R.drawable.f, "Do Ha", "Nghe An"));
+//        cardItems.add(new CardItem(R.drawable.g, "Dong Nhi", "Hue"));
+//        cardItems.add(new CardItem(R.drawable.e, "Le Quyen", "Sai Gon"));
+//        cardItems.add(new CardItem(R.drawable.c, "Phuong Linh", "Thanh Hoa"));
+//        cardItems.add(new CardItem(R.drawable.d, "Phuong Vy", "Hanoi"));
+//        cardItems.add(new CardItem(R.drawable.b, "Ha Ho", "Da Nang"));
+
+        cardsAdapter = new CardsAdapter(getActivity(), cardItems);
+        cardStack.setAdapter(cardsAdapter);
     }
+
+//    private void setfragment(Fragment page) {
+//
+//        FragmentTransaction fragmentTransaction =getFragmentManager().beginTransaction();
+//        fragmentTransaction.replace(R.id.matches_frame,page);
+//        fragmentTransaction.commit();
+
+//    }
 }
